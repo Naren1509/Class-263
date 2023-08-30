456fa1caeaf75e9124b2605c6d5e1536c51d2560
Naren1509 <143609768+Naren1509@users.noreply.github.com>
2023-08-30 17:38:53 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+#import flask library
+from flask import Flask,render_template,request
+#initialize flask
+app = Flask(__name__)
+#route your webpage
+@app.route("/")
+
+def visitors():
+
+	# Load current count
+	counter_read_file = open("count.txt", "r")
+	visitors_count = int(counter_read_file.read())
+	counter_read_file.close()
+
+	# Increment the count
+	visitors_count = visitors_count + 1
+
+	# Overwrite the count
+	counter_write_file = open("count.txt", "w")
+	counter_write_file.write(str(visitors_count))
+	counter_write_file.close()
+
+	return render_template("index.html",count=visitors_count)
+
+# Render HTML with count variable
+
+#route your webpage
+@app.route('/',methods=['POST'])
+def covid_stats():
+	# Load current count
+	counter_read_file = open("count.txt", "r")
+	visitors_count = int(counter_read_file.read())
+	counter_read_file.close()
+
+	text = request.form['text']
+	text1 = 'http://covidstats-sdbd.onrender.com/?country='+text
+	print(text1)
+	return render_template('index.html',image=text1,count=visitors_count)
+	#complete the code
+
+app.run()
+
+#add code for executing flask
